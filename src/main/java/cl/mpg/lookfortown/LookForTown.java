0d28686b6be3b72b5fae06115430a7e25c69b999
Matías Poirier <97362850+matipoirierg@users.noreply.github.com>
2023-07-15 22:51:45 -0400

    Add files via upload

---
+++ src/main/java/cl/mpg/lookfortown/LookForTown.java
@@ -0,0 +1,119 @@
+package cl.mpg.lookfortown;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import me.clip.placeholderapi.PlaceholderAPI;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public final class LookForTown extends JavaPlugin implements CommandExecutor {
+
+    private FileConfiguration messagesConfig;
+    private translateHexColorCodes hexTranslator;
+    private int cooldownDuration;
+    private String cooldownMessage;
+    private Map<UUID, Long> cooldowns;
+
+    @Override
+    public void onEnable() {
+        getCommand("lft").setExecutor(this);
+        getCommand("lftreload").setExecutor(this);
+        hexTranslator = new translateHexColorCodes();
+        loadMessagesConfig();
+
+        cooldownDuration = getConfig().getInt("cooldown-duration");
+        String cooldownMessageRaw = getConfig().getString("cooldown-message");
+        cooldownMessage = (cooldownMessageRaw != null) ? ChatColor.translateAlternateColorCodes('&', hexTranslator.translateHexColorCodes("&#", "", cooldownMessageRaw)) : "";
+
+        cooldowns = new HashMap<>();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("lftreload")) {
+            reloadConfig();
+            messagesConfig = getConfig();
+            sender.sendMessage(getMessage("reload"));
+            return true;
+        }
+
+        if (!command.getName().equalsIgnoreCase("lft")) {
+            return false;
+        }
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(getMessage("player-only"));
+            return true;
+        }
+
+        Player player = (Player) sender;
+        if (hasCooldown(player)) {
+            long remainingTime = getCooldownTime(player);
+            String formattedTime = formatTime(remainingTime);
+            player.sendMessage(cooldownMessage.replace("{time}", formattedTime));
+            return true;
+        }
+
+        String townName = PlaceholderAPI.setPlaceholders(player, "%townyadvanced_town%");
+        if (townName.equalsIgnoreCase("")) {
+            getServer().broadcastMessage(getMessage("broadcast-town").replace("{player}", player.getName()));
+            applyCooldown(player);
+        } else {
+            player.sendMessage(getMessage("in-town"));
+        }
+
+        return true;
+    }
+
+    private void loadMessagesConfig() {
+        File configFile = new File(getDataFolder(), "config.yml");
+
+        if (!configFile.exists()) {
+            saveResource("config.yml", false);
+        }
+
+        messagesConfig = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    private String getMessage(String key) {
+        if (messagesConfig == null) {
+            return null;
+        }
+
+        String message = messagesConfig.getString(key);
+        if (message != null && message.contains("&#")) {
+            message = hexTranslator.translateHexColorCodes("&#", "", message);
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    private boolean hasCooldown(Player player) {
+        return cooldowns.containsKey(player.getUniqueId());
+    }
+
+    private long getCooldownTime(Player player) {
+        long currentTime = System.currentTimeMillis();
+        long cooldownTime = cooldowns.get(player.getUniqueId());
+        return Math.max(0, (cooldownTime - currentTime) / 1000);
+    }
+
+    private void applyCooldown(Player player) {
+        long cooldownTime = System.currentTimeMillis() + (cooldownDuration * 1000);
+        cooldowns.put(player.getUniqueId(), cooldownTime);
+        Bukkit.getScheduler().runTaskLater(this, () -> cooldowns.remove(player.getUniqueId()), cooldownDuration * 20L);
+    }
+
+    private String formatTime(long time) {
+        long seconds = time % 60;
+        return String.valueOf(seconds);
+    }
+}
---
+++ src/main/java/cl/mpg/lookfortown/translateHexColorCodes.java
@@ -0,0 +1,20 @@
+package cl.mpg.lookfortown;
+import org.bukkit.ChatColor;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class translateHexColorCodes {
+    public String translateHexColorCodes(String startTag, String endTag, String message) {
+        final Pattern hexPattern = Pattern.compile(startTag + "([A-Fa-f0-9]{6})" + endTag);
+        Matcher matcher = hexPattern.matcher(message);
+        StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
+        while (matcher.find()) {
+            String group = matcher.group(1);
+            matcher.appendReplacement(buffer, ChatColor.COLOR_CHAR + "x"
+                    + ChatColor.COLOR_CHAR + group.charAt(0) + ChatColor.COLOR_CHAR + group.charAt(1)
+                    + ChatColor.COLOR_CHAR + group.charAt(2) + ChatColor.COLOR_CHAR + group.charAt(3)
+                    + ChatColor.COLOR_CHAR + group.charAt(4) + ChatColor.COLOR_CHAR + group.charAt(5)
+            );
+        }
+        return matcher.appendTail(buffer).toString();
+    }
+}
